8974825b63eb30786d29c4676baf09528bab0cb5
Blair <87805559+itsBlair-hide@users.noreply.github.com>
2022-09-13 07:11:33 +0800

    added chatBlockPandora.user.js

---
+++ chatBlockPandora.user.js
@@ -0,0 +1,58 @@
+/// ==UserScript==
+// @name         Block Pandora in chat
+// @namespace    https://github.com/itsBlair-hide/torn-userscripts
+// @version      0.1
+// @description  Deletes all messages posted by Pandora [1541749]
+// @author       Kwack [2190604]
+// @match        *://torn.com/*
+// @updateURL   "todo"
+// ==/UserScript==
+let chatRoot;
+let pandoraMessages;
+
+let GM_addStyle = function (s) {
+    let style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = s;
+    document.head.appendChild(style);
+};
+
+
+pageReady().then(() => {
+    let deleter = setInterval(() => {
+        pandoraMessages = chatRoot.querySelectorAll(
+            "[href='/profiles.php?XID=1541749'"
+        );
+
+        Array.from(pandoraMessages).forEach((msg) => {
+            msg.parentElement.classList.add("stfu");
+        });
+    });
+});
+
+async function pageReady() {
+    let checker = setInterval(() => {
+        if (document.querySelector("#chatRoot")) {
+            chatRoot = document.querySelector("#chatRoot");
+            console.log("Chat root found! Starting script now...");
+            clearInterval(checker);
+        }
+    });
+}
+
+var styles = `
+.stfu {
+    display: none !important
+}
+`;
+
+GM_addStyle(styles);
+
+function stopHiding() {
+    clearInterval(checker);
+    clearInterval(deleter);
+
+    [...document.querySelectorAll(".stfu")].forEach((msg) => {
+        msg.classList.remove("stfu")
+    })
+}
