e80575b675c5505b5923397d840bbe71cfe11242
Blair <87805559+Kwack-Kwack@users.noreply.github.com>
2022-10-25 02:05:29 +0800

    update testing script

--- other/PDA_TESTING_SCRIPT.user.js
+++ other/PDA_TESTING_SCRIPT.user.js
@@ -4,21 +4,27 @@
 // @version      0.1
 // @description  Downloads the script from the url below and evaluates it
 // @author       Kwack [2190604]
-// @match        https://www.torn.com/*
+// @match        https://www.torn.com/forums.php*
+// @grant        none
 // ==/UserScript==
 
-console.log("Test log")
 let url = "https://github.com/Kwack-Kwack/torn-userscripts/raw/main/other/forums%20crash%20issue/test.js";
-console.log(`URL for testing script: ${url}`)
+
+(async () => {
+    console.log("Test log")
+    console.log(`URL for testing script: ${url}`)
 
 let response;
-try {
-    response = await PDA_httpGet(url).then(r => r.responseText);
-} catch(e) {
-    console.error("Error thrown during fetch", e)
-}
+    try {
+        response = await PDA_httpGet(url).then(r => r.responseText);
+    } catch(e) {
+        console.error("Error thrown during fetch", e)
+    }
 
-console.log(`Script requested at ${url} contains the following code:`)
-console.log(response)
+    console.log(`Script requested at ${url} contains the following code:`)
+    console.log(response)
+
+})()
+
 
 // await PDA_evaluateJavascript(response)
