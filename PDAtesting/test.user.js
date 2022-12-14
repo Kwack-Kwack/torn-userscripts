bcd5d5a67a371fd6f14485e7c2575f7a6d898039
Blair <87805559+Kwack-Kwack@users.noreply.github.com>
2022-12-14 19:53:17 +0800

    testing

--- PDAtesting/test.user.js
+++ PDAtesting/test.user.js
@@ -1,1 +1,8 @@
-console.log(await PDA_httpGet("https://api.torn.com/key/?selections=info&key=WjPLZilBhEcALpLh").then(r => r.json())) //this apikey is public and temporary, nice try
+const response = await fetch("https://api.torn.com/key/?selections=info&key=WjPLZilBhEcALpLh")
+
+console.log(response)
+
+async function fetch(url) {
+    const r = await PDA_httpGet("https://api.torn.com/key/?selections=info&key=WjPLZilBhEcALpLh").then(r => r.json());
+    return r.json()
+}
