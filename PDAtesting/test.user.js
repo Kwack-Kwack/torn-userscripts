a5738a6819baa2e3e8bb96a36c82b609e180637d
Blair <87805559+Kwack-Kwack@users.noreply.github.com>
2022-12-14 19:49:31 +0800

    testing

--- PDAtesting/test.user.js
+++ PDAtesting/test.user.js
@@ -1,1 +1,1 @@
-console.log("###PDA-APIKEY###");
+console.log(await PDA_httpGet("https://api.torn.com/key/?selections=info&key=WjPLZilBhEcALpLh").then(r => r.json())) //this apikey is public and temporary, nice try
